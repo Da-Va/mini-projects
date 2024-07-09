1d9001e0907cf25e59fee69c5a00f326003c79f7
David Valouch <valoudav@fel.cvut.cz>
2024-07-09 10:59:41 +0200

    add moment arm

--- 2dof_leg_analysis/test.py
+++ 2dof_leg_analysis/test.py
@@ -4,34 +4,54 @@ import matplotlib.pyplot as plt
 import sys
 
 N=1000
+M = 2.5
 
 L1 = 1
-L2 = 0.8
+L2 = 1.0
 
 def ik(x, y):
-    t1 = (-x*x-y*y - L1**2 + L2**2)/(2*np.sqrt(x*x+y*y)*L1)
-    a1 = np.arctan2(y, x) + np.arccos(t1)
+    ds = x*x + y*y
+    c1 = (ds + L1**2 - L2**2)/(2*np.sqrt(ds)*L1)
+    a1 = np.arctan2(y, x) + np.arccos(c1)
 
-    t2 = (x*x + y*y - L1**2 - L2**2)/(2*L1*L2)
-    a2 = np.pi - np.arccos(t2)
+    c2 = (L1**2 + L2**2 - ds)/(2*L1*L2)
+    a2 = np.pi - np.arccos(c2)
     
     return a1, a2
 
 def main(argv):
-    XX, YY = np.meshgrid(np.linspace(-2, 2, N), np.linspace(-2, 2, N))
+    XX, YY = np.meshgrid(np.linspace(-M, M, N), np.linspace(-M, M, N))
     
     A1, A2 = ik(XX, YY)
     
-    # A1[A1>0] = np.nan
-    # A1[A1<-np.pi/2] = np.nan
+    Lx = np.abs(XX) + np.abs(XX - np.cos(A1))
+    Ly = np.abs(YY) + np.abs(YY - np.sin(A1))
     
-    ax = plt.subplot(1,2,1) 
+    # A2[A1>np.pi/2] = np.nan
+    # A2[A1<-0] = np.nan
+    # A1[A1>np.pi/2] = np.nan
+    # A1[A1<-0] = np.nan
+
+    Lx[np.isnan(A1)] = np.nan
+    Ly[np.isnan(A1)] = np.nan
+    
+    ax = plt.subplot(1,3,1) 
+    ax.axis('equal')
+    ax.scatter(0,0)
     c = ax.contourf(XX, YY, A1, levels=60)
     plt.colorbar(c, ax=ax)
     
-    ax = plt.subplot(1,2,2) 
-    c = ax.contourf(XX, YY, A2, levels=60)
+    ax = plt.subplot(1,3,2) 
+    ax.axis('equal')
+    c = ax.contourf(XX, YY, Lx, levels=60)
     plt.colorbar(c, ax=ax)
+    ax.scatter([0,1,1],[0,0,-1])
+
+    ax = plt.subplot(1,3,3) 
+    ax.axis('equal')
+    c = ax.contourf(XX, YY, Ly, levels=60)
+    plt.colorbar(c, ax=ax)
+    ax.scatter(0,0)
     
 
     plt.show()
